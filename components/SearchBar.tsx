9b5526bfbd28e8e76126ec31252cbd279a8e5e17
khanhthanhdev <thanhkt27507@gmail.com>
2023-06-13 20:38:01 +0700

    car details, add car image from api

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,10 +3,26 @@
 import { useState } from 'react'
 
 import {SearchManufacturer} from './'
+import Image from 'next/image'
+
+const SearchButton = ({otherClasses}: {otherClasses: string}) => (
+    <button type="submit"
+    className={`-ml-3 z-10 ${otherClasses}`}
+    >
+        <Image 
+            src="/magnifying-glass.svg"
+            alt="magnifying glass"
+            width={40}
+            height={40}
+            className='object-contain'
+        />
+    </button>
+)
+
 
 const SearchBar = () => {
 
-    const [manufacturer, setManufacturer] = useState('')
+    const [manufacturer, setManuFacturer] = useState('')
 
     const handleSearch = () => {}
 
@@ -17,7 +33,17 @@ const SearchBar = () => {
         <div className="searchbar__item">
             <SearchManufacturer 
                 manufacturer={manufacturer}
-                setManufacturer={setManufacturer}
+                setManuFacturer={setManuFacturer}
+            />
+            <SearchButton otherClasses="sm:hidden" />
+        </div>
+        <div className='searchbar__item'>
+            <Image 
+                src="/model-icon.png"
+                width={25}
+                height={25}
+                className='absolute w-[20px] h-[20px] ml-4'
+                alt="car model"
             />
         </div>
     </form>
